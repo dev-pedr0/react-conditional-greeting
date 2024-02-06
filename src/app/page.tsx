080692bdd62058a57fda58c2d9cb00f65b8abf91
Pedro Costa <pedro4costa@outlook.com>
2024-02-06 09:54:07 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,58 @@
+/*
+const Page = () => {
+  const fullTime = new Intl.DateTimeFormat('pt-br', {
+    timeStyle: 'short',
+    hour12: false
+  }).format();
+
+  const hour = new Date().getHours();
+  let greeting = '';
+
+  if(hour >= 0 && hour < 12) {
+    greeting = 'Bom dia';
+  } else if(hour >= 12 && hour < 18) {
+    greeting = 'Boa tarde';
+  } else if(hour >= 18 && hour <= 23) {
+    greeting = 'Boa noite';
+  }
+
+  return (
+    <div className="w-screen h-screen flex flex-col 
+    justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="text-9xl">
+        {fullTime}
+      </div>
+      <div className="text-5xl font-bold">
+        {greeting}
+      </div>
+    </div>
+  );
+}
+
+export default Page;
+*/
+
+const Page = () => {
+  const fullTime = new Intl.DateTimeFormat('pt-br', {
+    timeStyle: 'short',
+    hour12: false
+  }).format();
+
+  const hour = new Date().getHours();
+
+  return (
+    <div className="w-screen h-screen flex flex-col 
+    justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="text-9xl">
+        {fullTime}
+      </div>
+      <div className="text-5xl font-bold">
+        {hour >= 0 && hour <12 && "Bom dia"}
+        {hour >= 12 && hour <18 && "Boa tarde"}
+        {hour >= 18 && hour <23 && "Boa noite"}
+      </div>
+    </div>
+  );
+}
+
+export default Page;
